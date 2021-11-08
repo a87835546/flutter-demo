825fd8359477bda1ff9af6d68c5e131b43683f04
yicen <a87835546@gmail.com>
2021-11-08 11:48:18 +0800

    add:text field 简单使用

--- lib/home/stateful_use.dart
+++ lib/home/stateful_use.dart
@@ -7,14 +7,68 @@ class StatefulUse extends StatefulWidget {
 }
 
 class _statefulUse extends State<StatefulUse> {
+  String _string = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("有状态变化widget的使用"),
       ),
-      body: Center(
-        child: Text("有状态变化widget的使用"),
+      body: Container(
+        child: Column(
+          children: [
+            Text("有状态变化widget的使用"),
+            TextField(
+
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.greenAccent),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                labelText: "textfield 的简单使用",
+                labelStyle: TextStyle(color: Colors.black87),
+                hintText: "12323123",
+                hintStyle: TextStyle(color: Colors.grey),
+                helperText: "helperText 的使用",
+                helperMaxLines: 1,
+                helperStyle: TextStyle(color: Colors.black87),
+                errorText: "errorText 的使用",
+                errorMaxLines: 1,
+                errorStyle: TextStyle(color: Colors.redAccent),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                  borderRadius: BorderRadius.circular(3)
+                ),
+                suffix: Text("suffix"),
+                suffixIcon: Icon(Icons.ac_unit),
+                filled: true,
+                prefix: Text("prefix"),
+                prefixIcon: Icon(Icons.pregnant_woman),
+                focusColor: Colors.greenAccent
+
+              ),
+              controller: null,
+              keyboardType: TextInputType.phone,
+              obscureText: true, /// 是否是密码
+              /// 显示光标
+              showCursor: true,
+              maxLength: 199,
+              onChanged: (newValue){
+                print("textfield ----- onChanged : $newValue");
+                setState(() {
+                  _string = newValue;
+                });
+              },
+              onEditingComplete: (){
+                print("textfield ----- onEditingComplete");
+              },
+              onTap: (){
+                print("textfield ----- onTap");
+              },
+            ),
+            Text("text field 的内容:$_string 内容的长度：${_string.length} 总长度:${199}"),
+          ],
+        ),
       ),
     );
   }
