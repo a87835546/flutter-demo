069e318d75b16fe8778eaea6316fd03403613bab
yicen <a87835546@gmail.com>
2021-12-20 00:00:28 +0800

    update:点击背景移除键盘

--- lib/wallet/deposit_view.dart
+++ lib/wallet/deposit_view.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_demo/utils/app_singleton.dart';
 import 'package:flutter_demo/utils/color_util.dart';
 import 'package:flutter_demo/wallet/banding_phone_page.dart';
@@ -25,39 +26,45 @@ class _DepositViewState extends State<DepositView> {
   @override
   Widget build(BuildContext context) {
     return( AppSingleton.userInfoModel !=null && AppSingleton.userInfoModel!.mobile==null)? BandingPhonePage(): Scaffold(
-      body: Container(
-        color: ColorUtil.hexColor('0x1A1A1C'),
-        child: MediaQuery.removePadding(
-          removeTop: true,
-          context: context,
-          child: ListView(
-            // physics: const AlwaysScrollableScrollPhysics(),
-            children: [
-              DepositNaviBar(
-                balance: balance,
-                height: MediaQuery.of(context).padding.top,
-              ),
-              DepositSegmentView(
-                click: (value) {
-                  log("$value");
-                  _pageController.jumpToPage(value);
-                },
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height,
-                child: PageView(
-                  physics: NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  controller: _pageController,
-                  children: [
-                     DepositStylePage(type: DepositStylePageType.deposit,),
-                     DepositStylePage(type: DepositStylePageType.withdraw,),
-                     DepositTransactionPage(),
-                     DepositStylePage(type: DepositStylePageType.bank,),
-                  ],
+      body: GestureDetector(
+        onTap: (){
+          SystemChannels.textInput.invokeMethod(
+              'TextInput.hide');
+        },
+        child: Container(
+          color: ColorUtil.hexColor('0x1A1A1C'),
+          child: MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: ListView(
+              // physics: const AlwaysScrollableScrollPhysics(),
+              children: [
+                DepositNaviBar(
+                  balance: balance,
+                  height: MediaQuery.of(context).padding.top,
                 ),
-              )
-            ],
+                DepositSegmentView(
+                  click: (value) {
+                    log("$value");
+                    _pageController.jumpToPage(value);
+                  },
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  child: PageView(
+                    physics: NeverScrollableScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    controller: _pageController,
+                    children: [
+                      DepositStylePage(type: DepositStylePageType.deposit,),
+                      DepositStylePage(type: DepositStylePageType.withdraw,),
+                      DepositTransactionPage(),
+                      DepositStylePage(type: DepositStylePageType.bank,),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
